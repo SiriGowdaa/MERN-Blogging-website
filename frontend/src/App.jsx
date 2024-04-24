8d68894d50e70ecce763a1189596fcb51d78d51f
Sirisha <sirishaar47@gmail.com>
2024-04-24 19:20:31 +0530

    create server and connect to database

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/Projects.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Projects() {
+  return (
+    <div>Blogs</div>
+  )
+}
+
+export default Projects

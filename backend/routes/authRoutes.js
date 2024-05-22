ad56f8eb53f1d87f31cd3747bbe383f989fbdacc
Sirisha <sirishaar47@gmail.com>
2024-05-22 17:05:50 +0530

    create signin api route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { signup } from '../controllers/authController.js';
+import { signup, signIn } from '../controllers/authController.js';
 
 const router = express.Router();
 
 
 router.post('/signup', signup);
+router.post('/signin', signIn);
 
 export default router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -18,7 +19,9 @@ function App() {
         <Route path='/signup' element={<Signup />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/projects' element={<Projects />} />
+
       </Routes>
+        <Footer />
     </BrowserRouter>
   )
 }

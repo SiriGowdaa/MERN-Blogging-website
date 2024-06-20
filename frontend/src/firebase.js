d9abf58251139773d547d3f1d73be374f6ecb73a
Sirisha <sirishaar47@gmail.com>
2024-06-20 18:55:45 +0530

    authDomain

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -6,12 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-blog-5d09c.firebaseapp.com",
+  authDomain: "https://siris-blog.onrender.com/",
   projectId: "mern-blog-5d09c",
   storageBucket: "mern-blog-5d09c.appspot.com",
   messagingSenderId: "703647706224",
-  appId: "1:703647706224:web:d8d751b5bb671a2cc258b5"
+  appId: "1:703647706224:web:d8d751b5bb671a2cc258b5",
 };
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
